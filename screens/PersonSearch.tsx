0457bf632bbbbc59b9539150e137a7a22fa3c950
GuyMishan <gaiu01999@gmail.com>
2021-09-25 13:56:30 +0300

    gg

--- screens/PersonSearch.tsx
+++ screens/PersonSearch.tsx
@@ -15,7 +15,7 @@ import { Header } from "../components";
 
 const { width, height } = Dimensions.get("screen");
 
-const PersonSearch = ({ navigation }) => {
+const PersonSearch = ({ navigation }: { navigation: any }) => {
   const [user, SetUser] = useState([])
 
   const [search, setSearch] = useState('');
@@ -61,18 +61,44 @@ const PersonSearch = ({ navigation }) => {
       })
   }
 
+  async function CancelFriendRequest(userid) {
+    let tempfriendrequestid;
+    for (let i = 0; i < user.friendrequests_sent_data.length; i++) {
+      if (user.friendrequests_sent_data[i].recieved_user == userid) {
+        tempfriendrequestid = user.friendrequests_sent_data[i]._id
+      }
+    }
+    axios.post(`${api}/api/user/deletesentfriendrequest`, { userid: user._id, friendrequestid: tempfriendrequestid })
+      .then(response => {
+        axios.post(`${api}/api/user/deleterecievedfriendrequest`, { userid: userid, friendrequestid: tempfriendrequestid })
+          .then(response => {
+            axios.post(`${api}/api/friendrequest/remove/${tempfriendrequestid}`)
+              .then(response => {
+                Getuserbytoken()
+              })
+              .catch((error) => {
+                console.log(error);
+              })
+          })
+          .catch((error) => {
+            console.log(error);
+          })
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      console.log({ userid: userid, friendrequestid: tempfriendrequestid })
+  }
+
   async function onuserclick(userid1) {
-    navigation.navigate('Profile',{userid:userid1,})
+    navigation.navigate('Profile', { userid: userid1, })
   }
 
   function isuseralreadyfriendrequested(userid) { //states= {0=no friend whatsoever,1=i requested friend,2=he requested friend,3=already friends(unfriend)}
-    //check if userid is in user friendrequests already(sender/reciever) +another func for is the user is me?+another
-    if(user.friendrequests_sent_data.some(friendrequest_sent_data=>friendrequest_sent_data.recieved_user===userid))
-    {
+    if (user.friendrequests_sent_data.some(friendrequest_sent_data => friendrequest_sent_data.recieved_user === userid)) {
       return 1;
     }
-    if(user.friendrequests_recieved_data.some(friendrequest_recieved_data=>friendrequest_recieved_data.send_user===userid))
-    {
+    if (user.friendrequests_recieved_data.some(friendrequest_recieved_data => friendrequest_recieved_data.send_user === userid)) {
       return 2;
     }
     /*if(user.friends.some(friend=>friend._id===userid))
@@ -95,31 +121,31 @@ const PersonSearch = ({ navigation }) => {
                 <Text style={{ fontWeight: "bold" }}>{item.name}</Text>
               </View>
               <View style={{ justifyContent: 'center', alignItems: "center", flex: 1 }}>
-                {isuseralreadyfriendrequested(item._id) ==0 ? //more than true/false.. write down states!!!
+                {isuseralreadyfriendrequested(item._id) == 0 ? //more than true/false.. write down states!!!
                   <GaButton color="info" style={{ width: '80%', height: '40%' }} onPress={() => SendFriendRequest(item._id)}>
                     <Text bold>
                       Add Friend
                     </Text>
-                  </GaButton> 
-                  : isuseralreadyfriendrequested(item._id) ==1 ?  
-                  <GaButton color="#808080" style={{ width: '80%', height: '40%' }} /*onPress={() => CancelFriendRequest(item._id)}*/>
-                  <Text bold>
-                    Sent Request
+                  </GaButton>
+                  : isuseralreadyfriendrequested(item._id) == 1 ?
+                    <GaButton color="#808080" style={{ width: '80%', height: '40%' }} onPress={() => CancelFriendRequest(item._id)}>
+                      <Text bold>
+                        Sent Request
                   </Text>
-                </GaButton> 
-                : isuseralreadyfriendrequested(item._id) ==2 ?
-                <GaButton color="success" style={{ width: '80%', height: '40%' }} /*onPress={() => AcceptFriendRequest(item._id)}*/>
-                  <Text bold>
-                    Accept Request
+                    </GaButton>
+                    : isuseralreadyfriendrequested(item._id) == 2 ?
+                      <GaButton color="success" style={{ width: '80%', height: '40%' }} /*onPress={() => AcceptFriendRequest(item._id)}*/>
+                        <Text bold>
+                          Accept Request
                   </Text>
-                </GaButton> 
-                : isuseralreadyfriendrequested(item._id) ==3 ?
-                <GaButton color="error" style={{ width: '80%', height: '40%' }} /*onPress={() => UnFriendRequest(item._id)}*/>
-                  <Text bold>
-                    Unfriend
+                      </GaButton>
+                      : isuseralreadyfriendrequested(item._id) == 3 ?
+                        <GaButton color="error" style={{ width: '80%', height: '40%' }} /*onPress={() => UnFriendRequest(item._id)}*/>
+                          <Text bold>
+                            Unfriend
                   </Text>
-                </GaButton> 
-                :null }
+                        </GaButton>
+                        : null}
               </View>
             </TouchableOpacity>
             <Block style={{ borderColor: "rgba(0,0,0,0.2)", width: '100%', borderWidth: StyleSheet.hairlineWidth }} />
@@ -167,32 +193,32 @@ const PersonSearch = ({ navigation }) => {
 
   return (
     <>
-    <Header/>
-    <View style={{ flex: 1 }}>
-      <SafeAreaView style={{ flex: 1 }}>
-        <Input
-          right
-          color="black"
-          style={styles.search}
-          placeholder="Who are you looking for?"
-          placeholderTextColor={'#8898AA'}
-          iconContent={<Icon size={16} name="search-zoom-in" family="ArgonExtra" />}
-          onChangeText={(text) => searchFilterFunction(text)}
-          onClear={(text) => searchFilterFunction('')}
-          value={search}
-        />
-        <Block style={{ borderColor: "rgba(0,0,0,0.2)", width: '100%', borderWidth: StyleSheet.hairlineWidth }} />
-        {search.length >= 2 ?
-          <FlatList
-            style={{ flex: 1 }}
-            data={filteredDataSource}
-            renderItem={({ item }) => <Item item={item} />}
-            keyExtractor={item => item._id}
-          /> : null}
+      <Header />
+      <View style={{ flex: 1 }}>
+        <SafeAreaView style={{ flex: 1 }}>
+          <Input
+            right
+            color="black"
+            style={styles.search}
+            placeholder="Who are you looking for?"
+            placeholderTextColor={'#8898AA'}
+            iconContent={<Icon size={16} name="search-zoom-in" family="ArgonExtra" />}
+            onChangeText={(text) => searchFilterFunction(text)}
+            onClear={(text) => searchFilterFunction('')}
+            value={search}
+          />
+          <Block style={{ borderColor: "rgba(0,0,0,0.2)", width: '100%', borderWidth: StyleSheet.hairlineWidth }} />
+          {search.length >= 2 ?
+            <FlatList
+              style={{ flex: 1 }}
+              data={filteredDataSource}
+              renderItem={({ item }) => <Item item={item} />}
+              keyExtractor={item => item._id}
+            /> : null}
 
-      </SafeAreaView>
-      <Footer navigation={navigation} />
-    </View>
+        </SafeAreaView>
+        <Footer navigation={navigation} />
+      </View>
     </>
   );
 }
